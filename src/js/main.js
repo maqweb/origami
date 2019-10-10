26aee29ed0aa6d3e270e935e681b109029502111
maqweb <balikhinv@bk.ru>
2019-10-10 15:39:39 +0300

    arrows chenged

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,8 @@ $(document).ready(function () {
   $('#slider').slick({
     dots: true,
     // arrows: true,
-    nextArrow: '<img src="../img/vector-2.svg" alt="">',
-    prevArrow: '<img src="../img/vector.svg" alt="">',
+    nextArrow: '<img src="img/vector-2.svg" alt="">',
+    prevArrow: '<img src="img/vector.svg" alt="">',
   });
 
 });
