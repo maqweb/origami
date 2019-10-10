0652a4b47b2f1ab4d7316b7037fa0309f18f72a9
maqweb <balikhinv@bk.ru>
2019-10-10 15:30:38 +0300

    slide chenged

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
 $(document).ready(function () {
   
   $('#slider').slick({
-
+    dots: true,
+    // arrows: true,
+    nextArrow: '<img src="../img/vector-2.svg" alt="">',
+    prevArrow: '<img src="../img/vector.svg" alt="">',
   });
 
 });
