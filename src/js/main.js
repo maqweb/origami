942c67c54321d6e27439c0d1611097a0902487cc
maqweb <balikhinv@bk.ru>
2019-10-12 16:12:02 +0300

    +

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   
   $('#slider').slick({
     dots: true,
-    autoplay: true,
+    // autoplay: true,
     // arrows: true,
     nextArrow: '<img src="img/vector-2.svg" alt="">',
     prevArrow: '<img src="img/vector.svg" alt="">',
@@ -10,11 +10,32 @@ $(document).ready(function () {
 
   $('#reviews__wrap').slick({
     dots: true,
-    autoplay: true,
+    // autoplay: true,
     // arrows: true,
     nextArrow: '<img src="img/vector-2.svg" alt="">',
     prevArrow: '<img src="img/vector.svg" alt="">',
   });
+
+  $('.gallery').each(function () {
+    $(this).magnificPopup({
+      delegate: 'a', // the selector for gallery item
+      type: 'image',
+      gallery: {
+        enabled: true
+      },
+      markups: '<button title="%title%" class="mfp-arrow" style="position: absolute; top: 30px; right: -15px"><img src="img/vector.svg" width="25" height="29"/></button>',
+    })
+  })
+
+  // $('.popup-link').magnificPopup({
+  //   callbacks: {
+  //     open: function () {
+  //       $('.popup-slide').slick({
+  //         infinite: true
+  //       });
+  //     }
+  //   }
+  // });
 
 
   $('.call').click(function () {
@@ -24,4 +45,5 @@ $(document).ready(function () {
   $('#close_modal, #black_fill').click(function () {
     $('#modal_box, #black_fill').hide();
   })
+
 });
